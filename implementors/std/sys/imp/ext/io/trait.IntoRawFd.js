b7621fac4ef517c30295155c0d502b1ece13bb67
Federico Giraud <giraud.federico@gmail.com>
2017-06-22 09:23:48 +0100

    Update documentation

---
+++ implementors/std/sys/imp/ext/io/trait.IntoRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["scheduled_executor"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.IntoRawFd.html\" title=\"trait std::sys::imp::ext::io::IntoRawFd\">IntoRawFd</a> for <a class=\"struct\" href=\"https://docs.rs/mio/0.6.1/mio/net/tcp/struct.TcpStream.html\" title=\"struct mio::net::tcp::TcpStream\">TcpStream</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.IntoRawFd.html\" title=\"trait std::sys::imp::ext::io::IntoRawFd\">IntoRawFd</a> for <a class=\"struct\" href=\"https://docs.rs/mio/0.6.1/mio/net/tcp/struct.TcpListener.html\" title=\"struct mio::net::tcp::TcpListener\">TcpListener</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.IntoRawFd.html\" title=\"trait std::sys::imp::ext::io::IntoRawFd\">IntoRawFd</a> for <a class=\"struct\" href=\"https://docs.rs/mio/0.6.1/mio/net/udp/struct.UdpSocket.html\" title=\"struct mio::net::udp::UdpSocket\">UdpSocket</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/tokio_io/trait.AsyncRead.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["scheduled_executor"] = ["impl <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for <a class=\"struct\" href=\"https://docs.rs/tokio-core/0.1/tokio_core/net/tcp/struct.TcpStream.html\" title=\"struct tokio_core::net::tcp::TcpStream\">TcpStream</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for &amp;'a <a class=\"struct\" href=\"https://docs.rs/tokio-core/0.1/tokio_core/net/tcp/struct.TcpStream.html\" title=\"struct tokio_core::net::tcp::TcpStream\">TcpStream</a>","impl&lt;E&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for <a class=\"struct\" href=\"https://docs.rs/tokio-core/0.1/tokio_core/reactor/poll_evented/struct.PollEvented.html\" title=\"struct tokio_core::reactor::poll_evented::PollEvented\">PollEvented</a>&lt;E&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;E: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,&nbsp;</span>","impl&lt;'a, E&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-io/0.1/tokio_io/trait.AsyncRead.html\" title=\"trait tokio_io::AsyncRead\">AsyncRead</a> for &amp;'a <a class=\"struct\" href=\"https://docs.rs/tokio-core/0.1/tokio_core/reactor/poll_evented/struct.PollEvented.html\" title=\"struct tokio_core::reactor::poll_evented::PollEvented\">PollEvented</a>&lt;E&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;&amp;'a E: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
